ad2af7fad19f12f0161f1089e2b2df8ff5769107
Shaun Porter <shaunoman840@gmail.com>
2017-07-05 03:08:47 +0100

    Implemented GET methods for retreving sign-in data. Included linter
    api/user return JSON of signed in user data.
    
    api/user/name: Returns user name
    
    api/user/score: Returns user points
    
    View counter implemented. Crude currently and increments on any page
    fresh.
    
    Linter style is altered version of airbnb style, with personal tweaks
    included to fit modern coding habits and methods.
    
    Uncessessary CSS removed.

--- index.php
+++ index.php
@@ -1,30 +1,19 @@
 <?php
-// Import classes
-include "php/person.php";
-// Import vendor resources
-require 'vendor/autoload.php';
 
-// Show errors on PHP errors (By default these are hidden.. for some silly reason)
+require 'vendor/autoload.php';
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
-// Create reference to JWC databse
 $conn = new mysqli("localhost","root","toor","JWC");
-
 $app = new \Slim\App(["settings" => $config]);
-
 $container = $app->getContainer();
-
 $container['view'] = new \Slim\Views\PhpRenderer("./");
 
-//global $person;
-$person = new Person("shaun", 21);
-
 session_start();
-
-if( !isset($_SESSION["num"]) ){
-  $_SESSION["num"] = 0;
+if( !isset($_SESSION["user"]) ){
+  $_SESSION["user"] = null;
 }
+
 /*
   On inital request to server, render index.html page from templates folder.
 */
@@ -33,35 +22,75 @@ $app->get("/", function($request,$response){
   return $response;
 });
 
-$app->get("/test", function($req,$res){
-  $_SESSION["num"] = $_SESSION["num"] + 1;
-  return $res->getBody()->write($_SESSION["num"]);
-});
-
-$app->get("/mysql/test", function($req,$res){
-  // global $conn;
-  // $result = mysqli_query($conn,"SELECT * FROM horse");
-  // return $res->getBody()->write($result->lengths);
-});
-
-$app->get("/test/reset", function($req,$res){
-  $oldVal = $_SESSION["num"];
-  $_SESSION["num"] = 0;
-  return $res->getBody()->write("Test varable set to 0. Was : " . $oldVal);
-});
-
-$app->get("/api/connect", function($req,$res){
+//TODO: Convert to POST request
+//TODO: Remove loop implement WHERE in MySQL query
+$app->get("/api/user/login", function($req,$res){
   global $conn;
-  $dataSet = "";
-  if($result = $conn->query("SELECT * FROM horse")){
+  $loginName = $req->getQueryParams()["user"];
+  if($result = $conn->query("SELECT * FROM user")){
     while($row = $result->fetch_row()){
-      $dataSet = $dataSet . $row[1] . "</br>";
+      if($row[1] == $loginName){
+        $_SESSION["user"]["id"] = $row[0];
+        $_SESSION["user"]["name"] = $row[1];
+        $_SESSION["user"]["points"] = $row[2];
+        return $res->getBody()->write("User found!");
+      }
     }
   }
-  $res->getBody()->write($dataSet);
+  $res->getBody()->write("No user found.");
+  return $res->withStatus(404);
 });
 
+/*
+* Sets $_SESSION to null.
+* Logs out the user
+*/
+$app->get("/api/user/logout", function($req,$res){
+  $user = $_SESSION["user"]["name"];
+  $_SESSION["user"] = null;
+  return $res->getBody()->write($user . " sucessfully logged out.");
+});
 
+/*
+* Returns JSON object including user details
+*/
+$app->get("/api/user", function($req,$res){
+  if($_SESSION["user"] == null){
+    return $res->withStatus(404);
+  }
+  $user = ["user" =>["id" => $_SESSION["user"]["id"],
+                      "name" => $_SESSION["user"]["name"],
+                      "points" => $_SESSION["user"]["points"]
+                    ]
+          ];
+  return $res->getBody()->write(json_encode($user));
+});
+
+/*
+* Returns active user's name.
+*/
+$app->get("/api/user/name", function($req,$res){
+  if ($_SESSION["user"] == null){
+    return $res->withStatus(404);
+  }
+  return $res->getBody()->write($_SESSION["user"]["name"]);
+
+});
+
+/*
+* Returns active users score
+*/
+$app->get("/api/user/score", function($req,$res){
+  if($_SESSION["user"] == null){
+    return $res->withStatus("404");
+  }
+  return $res->getBody()->write($_SESSION["user"]["points"]);
+});
+
+/*
+* TODO: Set to increment per unique session
+* Increments the view counter stored as a local file: "/php/storage/viewcount.txt"
+*/
 $app->get("/api/viewcounter/new", function($req,$res){
   $counterFileURL = "./php/storage/viewcount.txt";
   $counterFile = fopen($counterFileURL, "r");
@@ -78,6 +107,9 @@ $app->get("/api/viewcounter/new", function($req,$res){
   $res->getBody()->write($counter + 1);
 });
 
+/*
+* Set viewcounter to 0
+*/
 $app->get("/api/viewcounter/reset", function($req,$res){
   $counterFileURL = "./php/storage/viewcount.txt";
   $counterFile = fopen($counterFileURL,"w");
@@ -86,19 +118,7 @@ $app->get("/api/viewcounter/reset", function($req,$res){
 
   $res->getBody()->write("View Counter reset!");
 });
-/*
-  Basic API Get request.
-  On /hello/"shaun" The server will respond with "Hello,shaun"
-*/
-$app->get('/hello/{name}', function ($request,$response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
 
-/*
-  Starts the server!
-*/
 $app->run();
 
 ?>
--- js/index.js
+++ js/index.js
@@ -1,50 +1,69 @@
-function showAdminForm(){
-  var adminLogin = document.getElementById("adminform");
-  var guestLogin = document.getElementById("guestform");
-  // Make adminLogin viewable and apply animation class
+/* jshint browser: true */
+
+function showAdminForm() {
+  const adminLogin = document.getElementById("adminform");
+  const guestLogin = document.getElementById("guestform");
+
   adminLogin.style.display = "inline-grid";
   adminLogin.className += " bounceInDown";
-  // Hide guestLogin element
   guestLogin.style.display = "none";
   guestLogin.classList.remove("bounceInDown");
 }
 
+/*
+* Placeholder function untill Guest form is implemented
+*/
 
-//TODO vvvvv Finish this! vvvvv
-function showGuestForm(){
-  var adminLogin = document.getElementById("adminform");
+function guestLoginTEST(guestName) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "/api/user/login?user=" + guestName);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log(guestName + " has logged in sucessfully!");
+      } else if (xhr.status === 404) {
+        console.log("Login Failure!");
+      }
+    }
+  };
+  xhr.send();
+}
 
 
+/*
+*vvvvv Finish this! vvvvv
+*/
+
+function showGuestForm() {
+  const adminLogin = document.getElementById("adminform");
 
   adminLogin.style.display = "none";
   adminLogin.classList.remove("bounceInDown");
 }
 
-function getViewCount(){
-  var viewCounter = document.getElementById("viewcounter");
-  var xhr = new XMLHttpRequest();
+function getViewCount() {
+  const viewCounter = document.getElementById("viewcounter");
+  const xhr = new XMLHttpRequest();
   xhr.open("GET", "/api/viewcounter/new");
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
       viewCounter.innerText = xhr.responseText;
     }
-  }
-
+  };
   xhr.send();
 }
 
-function testAPI(){
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET","/test");
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4){
+function testAPI() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "/test");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
       console.log(xhr.responseText);
     }
-  }
+  };
   xhr.send();
 }
 
-// Get View count on page load
 getViewCount();
 document.getElementById("btnLogin").addEventListener("click", showAdminForm);
-document.getElementById("btnSignup").addEventListener("click",testAPI);
+document.getElementById("btnSignup").addEventListener("click", testAPI);
